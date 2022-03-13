8d58e5fbe1dabaedbec38e7445bd3a98875205f6
efimovand <100632223+efimovand@users.noreply.github.com>
2022-03-13 04:01:32 +0400

    Added full achievements' list and ScrollView for them

--- SmokingApp/achievementsView.swift
+++ SmokingApp/achievementsView.swift
@@ -13,11 +13,11 @@ struct achievementsView: View {
     
     var body: some View {
         
-        NavigationView{
+        ZStack{
             
-            ZStack{
+            // Header + Achievements
+            VStack(spacing: 0){
                 
-                /*
                 // Header
                 ZStack{
                     
@@ -25,152 +25,87 @@ struct achievementsView: View {
                         .fill(Color(red: 1, green: 1, blue: 1, opacity: 0.40))
                         .frame(width: 377, height: 52)
                         .border(LinearGradient(gradient: Gradient(colors: [Color(red: 1, green: 1, blue: 1, opacity: 0.60), Color(red: 1, green: 1, blue: 1, opacity: 0.30)]), startPoint: .trailing, endPoint: .leading), width: 1)
-                        .offset(y: -355)
                     
                     Text("Достижения")
                         .foregroundColor(Color.white)
                         .font(.system(size: 28, weight: .heavy))
                         .multilineTextAlignment(.center)
                         .frame(width: 252, height: 27, alignment: .center)
-                        .offset(y: -355)
                     
                 }
-                 */
-                
-                /*
-                // NavigationBar
-                VStack{
-                    
-                    Spacer(minLength: 697)
-                    
-                    ZStack{
-                        
-                        HStack{
-                            // main
-                            Button(action: {
-                                self.selected = 0
-                            }) {
-                                if self.selected == 0
-                                {
-                                    Image("main_a")
-                                        .resizable()
-                                        .frame(width: 74, height: 74)
-                                        .offset(x: 1, y: -1)
-                                }
-                                else{
-                                    Image("main")
-                                        .resizable()
-                                        .frame(width: 74, height: 74)
-                                        .offset(x: 1, y: -1)
-                                }
-                            }
-                            
-                            Spacer(minLength: 10)
-                            
-                            // economy
-                            Button(action: {
-                                self.selected = 1
-                            }) {
-                                if self.selected == 1
-                                {
-                                    Image("economy_a")
-                                        .resizable()
-                                        .frame(width: 81, height: 81)
-                                        .offset(x: 1, y: -4)
-                                }
-                                else{
-                                    Image("economy")
-                                        .resizable()
-                                        .frame(width: 81, height: 81)
-                                        .offset(x: 1, y: -4)
-                                }
-                            }
-                            
-                            Spacer(minLength: 10)
-                            
-                            // achievements
-                            Button(action: {
-                                self.selected = 2
-                            }) {
-                                if self.selected == 2
-                                {
-                                    Image("achievements_a")
-                                        .resizable()
-                                        .frame(width: 73, height: 73)
-                                        .offset(x: 1)
-                                }
-                                else{
-                                    Image("achievements")
-                                        .resizable()
-                                        .frame(width: 73, height: 73)
-                                        .offset(x: 1)
-                                }
-                            }
-                            
-                            Spacer(minLength: 10)
-                            
-                            // restart
-                            Button(action: {
-                                self.selected = 3
-                            }) {
-                                if self.selected == 3
-                                {
-                                    Image("restart_test")
-                                        .resizable()
-                                        .frame(width: 67, height: 67)
-                                        .offset(x: 1)
-                                }
-                                else{
-                                    Image("restart")
-                                        .resizable()
-                                        .frame(width: 67, height: 67)
-                                        .offset(x: 1)
-                                }
-                            }
-                        }
-                        .padding(.horizontal, 50)
-                        .background(RoundedRectangle(cornerRadius: 20)
-                                        .fill(Color(red: 1, green: 1, blue: 1, opacity: 0.40))
-                                        .frame(width: 377, height: 92)
-                                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(LinearGradient(gradient: Gradient(colors: [Color(red: 1, green: 1, blue: 1, opacity: 0.50), Color(red: 1, green: 1, blue: 1, opacity: 0.50)]), startPoint: .trailing, endPoint: .leading), lineWidth: 1))
-                                        .offset(y: 7))
-                    }
-                }
-                 */
                 
                 
                 // Achievements
-                
-                /* ScrollView{ */
+                ScrollView(.vertical) {
                     
+                    // List
                     VStack(spacing: 15) {
                         
-                        achievement(name: "Начало положено", description: "Провести 1 день без курения", picture: Image("24hours"))
+                        Group {
+                            
+                            VStack(spacing: 15) {
+                                
+                                achievement(name: "Начало положено", description: "Провести 1 день без курения", picture: Image("24hours"))
+                                    .padding(.top, 19)
+                                
+                                achievement(name: "Первые дивиденды", description: "Сэкономить 500Р", picture: Image("coin"))
+                                
+                                achievement(name: "Идём на снижение", description: "Провести 3 дня без курения", picture: Image("nikotine"))
+                                
+                                achievement(name: "Денежное дерево", description: "Сэкономить 1000Р", picture: Image("moneytree"))
+                                
+                                achievement(name: "Неделя очищения", description: "Провести 7 дней без курения", picture: Image("wings"))
+                                
+                                achievement(name: "Спорт это жизнь", description: "Избавиться от одышки", picture: Image("walking"))
+                                
+                                achievement(name: "С кем не бывает", description: "Начать вторую попытку", picture: Image("heartballoon"))
+                                
+                                achievement(name: "Стонкс", description: "Сэкономить 2500Р", picture: Image("stonks"))
+                                
+                                achievement(name: "Железная выдержка", description: "Провести 14 дней без курения", picture: Image("yoga"))
+                                
+                                achievement(name: "Красивое зеркало", description: "Восстановить состояние кожи", picture: Image("beauty"))
+                                
+                            }
+                        }
                         
-                        achievement(name: "Первые дивиденды", description: "Сэкономить 500Р", picture: Image("coin"))
-                        
-                        achievement(name: "Идём на снижение", description: "Провести 3 дня без курения", picture: Image("nikotine"))
-                        
-                        achievement(name: "Денежное дерево", description: "Сэкономить 1000Р", picture: Image("moneytree"))
-                        
-                        achievement(name: "Неделя очищения", description: "Провести 7 дней без курения", picture: Image("wings"))
-                        
-                        achievement(name: "Спорт это жизнь", description: "Избавиться от одышки", picture: Image("walking"))
-                        
+                        Group {
+                            
+                            VStack(spacing: 15) {
+                                
+                                achievement(name: "Тяжелый бумажник", description: "Сэкономить 5000Р", picture: Image("wallet"))
+                                
+                                achievement(name: "Главное не сдаваться", description: "Начать пятую попытку", picture: Image("firststeps"))
+                                
+                                achievement(name: "Спокойной ночи", description: "Восстановить сон", picture: Image("sleep"))
+                                
+                                achievement(name: "Экономист", description: "Сэкономить 10000Р", picture: Image("treasure"))
+                                
+                                achievement(name: "Вдох выдох вдох", description: "Полностью очистить лёгкие от CO2", picture: Image("lungs"))
+                                
+                                achievement(name: "Это победа", description: "Провести 365 дней без курения", picture: Image("win"))
+                                    .padding(.bottom, 19)
+                                
+                            }
+                        }
                     }
-               /* } */
+                    
+                    
+                }.frame(width: 375, height: 632)
                 
-            }.background(Image("background")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 375, height: 812, alignment: .center)
-                            .edgesIgnoringSafeArea(.all))
+            }.padding(.top, -65)
             
-        }
+        }.background(Image("background")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 375, height: 812, alignment: .center)
+                        .edgesIgnoringSafeArea(.all))
+        
     }
 }
 
-// Every achievement's structure
+
+// Every achievement's structure (need add)
 struct achievement: View {
     
     var name: String
@@ -181,28 +116,27 @@ struct achievement: View {
         
         ZStack{
             
-            // right
-            RoundedCorners(tl: 0, tr: 10, bl: 0, br: 10).fill(Color(red: 1, green: 1, blue: 1, opacity: 0.40))
-                .frame(width: 270, height: 75)
-                .offset(x: 40)
-            
-            // left
-            RoundedCorners(tl: 10, tr: 0, bl: 10, br: 0).fill(Color(red: 0.89, green: 0.93, blue: 1, opacity: 0.40))
-                .frame(width: 75, height: 75)
-                .offset(x: -132)
+            HStack(alignment: .center, spacing: 0){
+                
+                // left
+                RoundedCorners(tl: 10, tr: 0, bl: 10, br: 0).fill(Color(red: 0.89, green: 0.93, blue: 1, opacity: 0.40))
+                    .frame(width: 75, height: 75)
+                
+                // right
+                RoundedCorners(tl: 0, tr: 10, bl: 0, br: 10).fill(Color(red: 1, green: 1, blue: 1, opacity: 0.40))
+                    .frame(width: 270, height: 75)
+                
+            }
             
             // text
-            VStack(
-                alignment: .leading,
-                spacing: 5
-            ) {
+            VStack(alignment: .leading, spacing: 5) {
                 
                 // name
                 Text(name)
                     .foregroundColor(Color.white)
                     .font(.system(size: 24, weight: .bold))
                     .multilineTextAlignment(.leading)
-                    .frame(width: 252, height: 27, alignment: .topLeading)
+                    .frame(width: 255, height: 27, alignment: .topLeading)
                 
                 // description
                 Text(description)
@@ -217,7 +151,7 @@ struct achievement: View {
             picture
                 .resizable()
                 .frame(width: 75, height: 75)
-                .offset(x: -133, y: -3)
+                .offset(x: -135, y: 0)
             
             // progressBar
             RoundedRectangle(cornerRadius: 10)
