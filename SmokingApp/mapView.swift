d22d10e801e9f65f5622d7269bf3415052e1c0d9
efimovand <100632223+efimovand@users.noreply.github.com>
2022-04-06 23:05:53 +0400

    Made mapPoint struct and removed ScrollView bounce

--- SmokingApp/mapView.swift
+++ SmokingApp/mapView.swift
@@ -33,7 +33,7 @@ struct mapView: View {
                     // mapBackground
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.red)
-                        .frame(width: 900, height: 1200)
+                        .frame(width: 1200, height: 1200)
                     
                     
                     // 1 day
@@ -49,7 +49,7 @@ struct mapView: View {
                             
                         }
                         
-                    }.offset(x: -350, y: 500)
+                    }.offset(x: -450, y: 500)
                     .opacity(testScore >= 1 ? 1 : 0)
                     
                     
@@ -79,7 +79,7 @@ struct mapView: View {
                             .rotationEffect(.degrees(45))
                             .offset(x: -115, y: 105)
                         
-                    }.offset(x: -150, y: 350)
+                    }.offset(x: -250, y: 350)
                     .opacity(testScore >= 2 ? 1 : 0)
                     
 
@@ -87,10 +87,11 @@ struct mapView: View {
                     .scaleEffect(x: -1.0, y: 1.0, anchor: .center)
                 
             }.frame(width: 315, height: 430)
-                .background(Color.red)
+                .background(Color.white)
                 .cornerRadius(10)
                 .rotationEffect(Angle(degrees: 180))
                 .scaleEffect(x: -1.0, y: 1.0, anchor: .center)
+                .modifier(SomeModifier())
             
             
         }
@@ -178,10 +179,177 @@ struct youHere: View {
 }
 
 
+struct mapPoint: View {
+    
+    @EnvironmentObject var data: UserData
+    
+    @State var picture: Image
+    @State var name: String
+    @State var description: String
+    @State var textShown: Bool = false
+    @State var number1: Int
+    @State var number2: Int
+    
+    @State var ringOpacity: CGFloat = 0.5
+    
+    @State var testScore: Int = 4
+    
+    var body: some View{
+        
+//        if (data.maxScoreHours / 24) >= number 1 {
+        if testScore >= number1 {
+            
+        ZStack{
+            
+            // bottom ring
+            Ellipse()
+                .frame(width: 65, height: 35)
+                .opacity(0)
+                .overlay(Ellipse().stroke(Color("a39cf4"), lineWidth: 15).opacity(ringOpacity).onChange(of: textShown, perform: { value in
+                        switch value {
+                        case false : withAnimation(.easeIn(duration: 0.2)) { ringOpacity = 0.5 }
+                        case true: withAnimation(.easeInOut(duration: 0.3)) { ringOpacity = 1 }
+                        }
+                }))
+            
+            // main button
+                ZStack{
+                    
+                    // shape
+                    RoundedCorners(tl: 10, tr: 10, bl: 0, br: 0)
+                        .fill(Color.red)
+                        .opacity(0)
+                        .overlay(RoundedCorners(tl: 10, tr: 10, bl: 0, br: 0).stroke(Color.white, lineWidth: 5))
+                        .frame(width: 65, height: 65)
+                    
+                    picture
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white).opacity(0.4).frame(width: 65, height: 65).overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)))
+                    
+                    Triangle()
+                        .foregroundColor(Color.white)
+                        .frame(width: 70.6, height: 20)
+                        .rotationEffect(.degrees(180))
+                        .offset(y: 45.1)
+                    
+                }.offset(y: -60)
+                .onTapGesture(perform: {
+                    textShown.toggle()
+                })
+
+            
+            if textShown {
+                
+                VStack(spacing: 0){
+                    
+                    ZStack{
+                        
+                        RoundedCorners(tl: 10, tr: 10, bl: 0, br: 0)
+                            .fill((Color.white).opacity(0.8))
+                            .frame(width: 250, height: 40)
+                        
+                        Text(name)
+                            .font(.system(size: 13, weight: .bold))
+                            .frame(width: 250, height: 35, alignment: .center)
+                        
+                    }
+                    
+                    ZStack{
+                        
+                        RoundedCorners(tl: 0, tr: 0, bl: 10, br: 10)
+                            .fill((Color.white).opacity(0.6))
+                            .frame(width: 250, height: 90)
+                        
+                        Text(description)
+                            .font(.system(size: 11, weight: .semibold))
+                            .frame(width: 235, height: 80, alignment: .topLeading)
+                        
+                        // days value
+                        ZStack{
+                            
+                            // background
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color("a39cf4"))
+                                .frame(width: 70, height: 28)
+                            
+                            // text
+                            HStack(spacing: 2){
+                                
+                                Text(String(number1))
+                                    .font(.system(size: 14, weight: .bold))
+                                
+                                if ((number1 != 11) && (number1 % 10 == 1)){
+                                    Text("день")
+                                        .font(.system(size: 12, weight: .bold))
+                                }
+                                else if (((number1 != 12) && (number1 != 13) && (number1 != 14)) && ((number1 % 10 == 2) || (number1 % 10 == 3) || (number1 % 10 == 4))){
+                                    Text("дня")
+                                        .font(.system(size: 12, weight: .bold))
+                                }
+                                else{
+                                    Text("дней")
+                                        .font(.system(size: 12, weight: .bold))
+                                }
+                                
+                            }
+                            
+                        }.offset(x: 90, y: 30)
+                        
+                        
+                        
+                    }
+                    
+                }.offset(y: -150)
+                
+            }
+            
+        }
+            
+        }
+        
+        else {
+            
+            //
+            
+        }
+        
+    }
+    
+}
+
+
+// triangle shape
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        return path
+    }
+}
+
+
+// modifier for disabling ScrollView bounce
+struct SomeModifier: ViewModifier {
+    init() {
+      UIScrollView.appearance().bounces = false
+    }
+
+    func body(content: Content) -> some View {
+        return content
+    }
+}
+
 struct mapView_Previews: PreviewProvider {
     static var previews: some View {
-        mapView(mapShown: .constant(false))
-            .environmentObject(UserData())
+//        mapView(mapShown: .constant(false))
+//            .environmentObject(UserData())
+//            .preferredColorScheme(.dark)
+        mapPoint(picture: Image("lungs"), name: "Нормализуется уровень углекислого газа в крови", description: "Организм избавляется от избытка CO2. Повышенное содержание кислорода помогает питать ткани и кровеносные сосуды, которые получали меньше кислорода во время курения.", number1: 4, number2: 6)
             .preferredColorScheme(.dark)
+            .environmentObject(UserData())
     }
 }
