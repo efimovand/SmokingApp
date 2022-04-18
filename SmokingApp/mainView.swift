38299645dcdfe9cb82f280da9df338a97f301672
efimovand <100632223+efimovand@users.noreply.github.com>
2022-04-18 22:49:41 +0400

    Made the big research about quitting nicotine. Added full list of healthNow cases and debugged it.

--- SmokingApp/mainView.swift
+++ SmokingApp/mainView.swift
@@ -11,9 +11,9 @@ import Foundation
 struct mainView: View {
     
     @EnvironmentObject var data: UserData
-//    @State var savedHours = UserDefaults.standard.object(forKey: "savedHours") as! Date
-//    @State var saved = UserDefaults.standard.object(forKey: "savedTime") as! Date
-//    @State var now = Date()
+    @State var savedHours = UserDefaults.standard.object(forKey: "savedHours") as! Date
+    @State var saved = UserDefaults.standard.object(forKey: "savedTime") as! Date
+    @State var now = Date()
     
     @State var height: Float = Float(UIScreen.screenHeight)
     @State var blurRadius : CGFloat = 0
@@ -42,35 +42,72 @@ struct mainView: View {
         // 4 days [6]
         healthNow(text: "Запасы никотина в организме истощаются", picture: Image("nicotine"), description: "Вы можете стать более раздражительным, может возникать головная боль, но это все — признаки восстановления организма."),
         
-        // 6 days [7]
-        healthNow(text: "Улучшается кровообращение и работа легких", picture: Image("lungs"), description: "-"),
+        // 5 days [7]
+        healthNow(text: "Восстанавливаются защитные механизмы", picture: Image("virus"), description: "Увеличивается уровень таких защитных антиоксидантов в крови, как витамин С и E. Это помогает организму бороться с возможными заболеваниями."),
         
-        // 7 days [8]
+        // 6 days [8]
+        healthNow(text: "Улучшается кровообращение и работа легких", picture: Image("medicalreport"), description: "Организм начинает улучшать кровообращение и комплексно восстанавливать легкие. Изменение кровообращения положительно влияет на все системы организма."),
+        
+        // 7 days [9]
         healthNow(text: "Зависимость ослабевает", picture: Image("handcuffs"), description: "Этап в одну неделю важен не только для вашего здоровья, но и для успешного отказа от курения в долгосрочной перспективе. Курильщики, которые успешно выдерживают одну неделю без курения, в девять раз чаще успешно бросают курить."),
         
-        // 14 days [9]
-        healthNow(text: "Повседневные физические нагрузки проходят легче", picture: Image("walking"), description: "Это происходит благодаря улучшению кровообращения и оксигенации, так как функционирование легких улучшается примерно на 30 процентов - университет Мичигана."),
+        // 8...9 days [10]
+        healthNow(text: "Увеличивается оборот клеток кожи", picture: Image("moleculas"), description: "По мере увеличения оборота клеток ваша кожа начинает восстанавливаться. Это происходит потому, что теперь к вашей коже притекает гораздо больше крови. Увеличение оборота клеток кожи помогает уменьшить появление морщин и улучшает способность вашей кожи заживлять шрамы от акне."),
         
-        // 30 days [10]
+        // 10...11 days [11]
+        healthNow(text: "Облегчается синдром сухого глаза", picture: Image("eye"), description: "Курение удваивает риск развития синдрома сухого глаза — распространенного заболевания, вызванного недостаточной влажностью поверхности глаз. Дым содержит токсины, которые раздражают глаза, что может вызвать и усугубить этот синдром, особенно если вы носите контактные линзы."),
+        
+        // ???
+        
+        // 14...15 days [12]
+        healthNow(text: "Повседневные физические нагрузки проходят легче", picture: Image("walking_2"), description: "Это происходит благодаря улучшению кровообращения и оксигенации, так как функционирование легких улучшается примерно на 30 процентов."),
+        
+        // 16...17 days [13]
+        healthNow(text: "Улучшается цвет кожи", picture: Image("beauty_2"), description: "Увеличение кровотока делает ваш цвет лица менее серым и бледным, что является одним из наиболее заметных различий в вашей коже до и после отказа от курения. По мере того, как ваша кожа получает больше питательных веществ и кислорода, ваше лицо может приобретать здоровый блеск."),
+        
+        // 18...19 days [14]
+        healthNow(text: "Снижается уровень тревоги", picture: Image("meditation_1"), description: "Никотин играет большую роль в развитии беспокойства. Также некоторые исследования утверждают, что курильщики более склонны к развитию депрессии, чем некурящие люди. В данный период отмены возможный уровень тревоги спадает, а симптомы абстиненции облегчаются."),
+        
+        // 20...21 days [15]
+        healthNow(text: "Восстанавливается сон", picture: Image("sleep"), description: "Нарушения сна, вызванные синдромом отмены, постепенно начинают проходить. Нервная система восстанавливается, улучшая сон и помогая организму в борьбе с возможной бессонницей."),
+        
+        // ???
+        
+        // 30...32 days [16]
         healthNow(text: "Восстанавливается нормальное функционирование легких", picture: Image("lungs"), description: "Уменьшается кашель и одышка. Повышается физическая выносливость организма."),
         
-        // 30 days [11]
+        // 33...35 days [17]
         healthNow(text: "Повышается уровень энергии", picture: Image("battery"), description: "Вы также можете заметить, что многие симптомы, связанные с курением, уменьшились. Например, заложенность носовых пазух и одышка при физических нагрузках."),
         
-        // 30 days [12]
-        healthNow(text: "Укрепление иммунитета", picture: Image("virus"), description: "Волокна в легких, которые помогают поддерживать здоровье легких, отрастают. Эти волокна помогают уменьшать избыточное накопление слизи и защищают от бактериальных инфекций."),
+        // 36...39 days [18]
+        healthNow(text: "Укрепление иммунитета", picture: Image("shield"), description: "Волокна в легких, которые помогают поддерживать здоровье легких, отрастают. Эти волокна помогают уменьшать избыточное накопление слизи и защищают от бактериальных инфекций."),
         
-        // 30-90 days [13]
-        healthNow(text: "Кислородный обмен продолжает улучшаться", picture: Image("medicalreport"), description: "-"),
+        // 40...42 days [19]
+        healthNow(text: "Улучшается общее качество сна", picture: Image("bed"), description: "Курение снижает глубину медленных фаз, которые помогают организму эффективно восполнять силы. После восстановления возможных нарушений сна, связанных с синдромом отмены, организм улучшает общее качество сна. Кроме того, уменьшаются проблемы с долгим засыпанием и апноэ."),
         
-        // 90 days [14]
+        // 43...45 days [20]
+        healthNow(text: "Кислородный обмен продолжает улучшаться", picture: Image("dizzyhead"), description: "Этот процесс помогает избавиться от общего недомогания, слабости, ухудшенной умственной деятельности, апатии и головокружения."),
+        
+        // 46...48 days [21]
+        healthNow(text: "Восстанавливается кожа", picture: Image("beauty"), description: "Курение снижает выработку организмом коллагена и эластина, двух белков, которые сохраняют упругость и эластичность кожи. Теперь ваша кожа начинает улучшаться, поскольку коллаген и эластин обновляются в этот период."),
+        
+        // 49...51 days [22] (NEW 9)
+        healthNow(text: "Восстанавливаются десны", picture: Image("teeth"), description: "Курение снижает количество кислорода, переносимого кровью, что затрудняет борьбу организма с возможными воспалениями десен. Кроме того, тепло от курения вызывает повреждение десен и горла, воспаляя их. Теперь десны комплексно восстанавливаются, избавляясь от возможных заболеваний, связанных с курением."),
+        
+        // 52...54 days [23]
+        healthNow(text: "Снижается риск глазных заболеваний", picture: Image("eyecare"), description: "Курение увеличивает количество свободных радикалов в глазах. Свободные радикалы несут ответственность за повреждение липидов и белков и вызывают образование отложений на поверхности хрусталика глаза, что приводит к развитию катаракты. В этот период отказа антиоксиданты, потребляемые с пищей, успешно борются с оставшимися свободными радикалами, значительно снижая риск возникновения катаракты."),
+        
+        // 90 days [24]
         healthNow(text: "Увеличивается уровень фертильности", picture: Image("baby"), description: "Репродуктивная функция улучшается, снижается риск преждевременных родов."),
         
-        // 180 days [15]
+        // 180 days [25]
         healthNow(text: "Повышается стрессоустойчивость", picture: Image("yoga"), description: "Вы можете переносить стрессовые ситуации без потребности в курении."),
         
-        // 180 days [16]
+        // 180 days [26]
         healthNow(text: "Снижается уровень слизи и мокроты в легких", picture: Image("lungs"), description: "Дыхательные пути гораздо меньше воспаляются без постоянного воздействия и содержащихся в нем химических веществ."),
+        
+        // 180 days [27]
+        healthNow(text: "Улучшается выработка глюкозы", picture: Image("honey"), description: "Благодаря снижению уровня никотина и окиси углерода в организме  улучшается выработка глюкозы. Это также позволяет организму более эффективно контролировать уровень сахара в крови у диабетиков."),
         
         
     ]
@@ -78,6 +115,7 @@ struct mainView: View {
     var body: some View {
         
         ZStack{
+            
             
             // Main
             VStack(spacing: 0){
@@ -211,7 +249,10 @@ struct mainView: View {
                 }
             })
             
+            
+            
             Spacer(minLength: UIScreen.screenHeight * 0.09)
+            
             
             // healthNow
             ZStack{
@@ -233,19 +274,44 @@ struct mainView: View {
                     healthCase[5]
                 case 4:
                     healthCase[6]
-                case 6:
+                case 5:
                     healthCase[7]
-                case 7:
+                case 6:
                     healthCase[8]
-                case 14:
+                case 7:
                     healthCase[9]
-                case 30:
-                    healthCase[10] // 10 or 11 or 12
-                    // case 30...90: healthCase[13]
-                case 90:
+                case 8...9:
+                    healthCase[10]
+                case 10...11:
+                    healthCase[11]
+                case 14...15:
+                    healthCase[12]
+                case 16...17:
+                    healthCase[13]
+                case 18...19:
                     healthCase[14]
+                case 19...20:
+                    healthCase[15]
+                case 30...32:
+                    healthCase[16]
+                case 33...35:
+                    healthCase[17]
+                case 36...39:
+                    healthCase[18]
+                case 40...42:
+                    healthCase[19]
+                case 43...45:
+                    healthCase[20]
+                case 46...48:
+                    healthCase[21]
+                case 49...51:
+                    healthCase[22]
+                case 52...54:
+                    healthCase[23]
+                case 90:
+                    healthCase[24]
                 case 180:
-                    healthCase[15] // 15 or 16
+                    healthCase[25] // 25 or 26 or 27
                 default:
                     healthCase[0]
                     
@@ -254,12 +320,14 @@ struct mainView: View {
             }.frame(height: 666, alignment: .center)
                 .clipped()
             
+            
             // statsNow
             ZStack{
                 
-                statsNow()
-                
+                //statsNow()
+            
             }.frame(width: UIScreen.screenWidth, alignment: .trailing)
+            
             
         }.ignoresSafeArea()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -270,27 +338,27 @@ struct mainView: View {
                         .edgesIgnoringSafeArea(.all)
                         .blur(radius: data.healthShown ? 3 : 0))
         .statusBar(hidden: height >= 812 ? false : true)
-//           .onAppear(perform: {
-//
-//                if data.hours > 24 {
-//                    UserDefaults.standard.set(false, forKey: "firstDay")
-//                    data.firstDay = false
-//                }
-//                else if (abs(savedHours - now)) > 3600 {
-//                    data.hours += Int((abs(savedHours - now)) / 3600)
-//                    savedHours = Date()
-//                }
-//
-//                if (abs(saved - now)) > 86400 {
-//                    data.score += Int((abs(saved - now)) / 86400)
-//                    saved = Date()
-//                }
-//
-//               if data.hours >= data.maxScoreHours {
-//                   data.maxScoreHours = data.hours
-//               }
-//
-//            })
+           .onAppear(perform: {
+
+                if data.hours > 24 {
+                    UserDefaults.standard.set(false, forKey: "firstDay")
+                    data.firstDay = false
+                }
+                else if (abs(savedHours - now)) > 3600 {
+                    data.hours += Int((abs(savedHours - now)) / 3600)
+                    savedHours = Date()
+                }
+
+                if (abs(saved - now)) > 86400 {
+                    data.score += Int((abs(saved - now)) / 86400)
+                    saved = Date()
+                }
+
+               if data.hours >= data.maxScoreHours {
+                   data.maxScoreHours = data.hours
+               }
+
+            })
         
     }
     
@@ -339,11 +407,28 @@ struct healthNow: View {
                             .frame(width: 200, height: 80, alignment: .leading)
                             .offset(x: 40)
                         
-                    case 11...23: Text(text)
+                    case 11...23:
+                        if (text != "Восстанавливается сон" && text != "Восстанавливается кожа" && text != "Восстанавливаются десны") {
+                            Text(text)
                             .font(.system(size: 22, weight: .bold))
                             .foregroundColor(Color.white)
                             .frame(width: 200, height: 80, alignment: .leading)
                             .offset(x: 40)
+                        }
+                        if (text != "Восстанавливаются десны") {
+                            Text(text)
+                            .font(.system(size: 22, weight: .bold))
+                            .foregroundColor(Color.white)
+                            .frame(width: 215, height: 80, alignment: .leading)
+                            .offset(x: 40)
+                        }
+                        else {
+                            Text(text)
+                            .font(.system(size: 22, weight: .bold))
+                            .foregroundColor(Color.white)
+                            .frame(width: 217, height: 80, alignment: .leading)
+                            .offset(x: 37)
+                        }
                         
                     case 24...33: Text(text)
                             .font(.system(size: 20, weight: .bold))
@@ -424,7 +509,7 @@ struct healthNow: View {
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.leading)
                         .frame(width: 295, height: 100, alignment: .top)
-                        .offset(y: 7)
+                        .offset(y: 2)
                         .opacity(descriptionTextOpacity)
                         .onChange(of: data.healthShown, perform: { value in
                             switch value {
@@ -486,6 +571,65 @@ struct healthNow: View {
                                 case true: withAnimation(.easeInOut(duration: 0.9)) { descriptionTextOpacity = 1 }
                                 }
                             })
+                        
+                    case 231...280:
+                        
+                        ScrollView([.vertical], showsIndicators: false) {
+                        
+                        Text(description)
+                            .font(.system(size: 13, weight: .semibold))
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 295, height: 120, alignment: .top)
+                            .opacity(descriptionTextOpacity)
+                            .onChange(of: data.healthShown, perform: { value in
+                                switch value {
+                                case false : withAnimation(.linear(duration: 0.35)) { descriptionTextOpacity = 0 }
+                                case true: withAnimation(.easeInOut(duration: 0.9)) { descriptionTextOpacity = 1 }
+                                }
+                            })
+                            
+                        }.frame(width: 295, height: 100)
+                        
+                    case 281...305:
+                        
+                        ScrollView([.vertical], showsIndicators: false) {
+                        
+                        Text(description)
+                            .font(.system(size: 12.8, weight: .semibold))
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 295, height: 140, alignment: .top)
+                            .offset(y: 1)
+                            .opacity(descriptionTextOpacity)
+                            .onChange(of: data.healthShown, perform: { value in
+                                switch value {
+                                case false : withAnimation(.linear(duration: 0.35)) { descriptionTextOpacity = 0 }
+                                case true: withAnimation(.easeInOut(duration: 0.9)) { descriptionTextOpacity = 1 }
+                                }
+                            })
+                        
+                    }.frame(width: 295, height: 100)
+                        
+                    case 306...395:
+                            
+                        ScrollView([.vertical], showsIndicators: false) {
+                            
+                        Text(description)
+                            .font(.system(size: 12.5, weight: .semibold))
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 295, height: 170, alignment: .top)
+                            .offset(y: 2)
+                            .opacity(descriptionTextOpacity)
+                            .onChange(of: data.healthShown, perform: { value in
+                                switch value {
+                                case false : withAnimation(.linear(duration: 0.35)) { descriptionTextOpacity = 0 }
+                                case true: withAnimation(.easeInOut(duration: 0.9)) { descriptionTextOpacity = 1 }
+                                }
+                            })
+                            
+                        }.frame(width: 295, height: 100)
                         
                     default: Text(description)
                             .font(.system(size: 14.5, weight: .semibold))
